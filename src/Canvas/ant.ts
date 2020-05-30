6036dd4b0cb416fa05be26ba995eb22f8eeb7baf
Suniron <etienne.blanc94@gmail.com>
2020-05-31 00:13:19 +0200

    imporve ant

--- src/Canvas/ant.ts
+++ src/Canvas/ant.ts
@@ -1,18 +1,44 @@
 import Cell from "./cell";
 
-type AntColor = "white" | "black" | "red";
 type Direction = "top" | "bottom" | "left" | "right";
 export class Ant {
   /** Cell object where is this Ant */
   cell: Cell;
-  color: AntColor;
+  color = "red";
   direction: Direction = "top"; // TODO: random on init
-  constructor(cell: Cell, color: AntColor) {
+  constructor(cell: Cell) {
     this.cell = cell;
-    this.color = color;
   }
 
   move = () => {
-    console.log("Ant move !");
+    // Change direction:
+    this.switchDirection();
+  };
+
+  /** Change direction depend of cell color and current direction */
+  switchDirection = () => {
+    if (this.cell.color === "white") {
+      switch (this.direction) {
+        case "top":
+          return (this.direction = "right");
+        case "right":
+          return (this.direction = "bottom");
+        case "bottom":
+          return (this.direction = "left");
+        case "left":
+          return (this.direction = "top");
+      }
+    } else if (this.cell.color === "black") {
+      switch (this.direction) {
+        case "top":
+          return (this.direction = "left");
+        case "left":
+          return (this.direction = "bottom");
+        case "bottom":
+          return (this.direction = "right");
+        case "right":
+          return (this.direction = "top");
+      }
+    }
   };
 }
